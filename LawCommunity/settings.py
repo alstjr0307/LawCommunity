3eaf7f0f07c35ca7ad4120cdcb4c693ccfb0f9a9
alstjr0307 <alswp07@naver.com>
2024-01-10 02:02:26 +0900

    123

--- LawCommunity/settings.py
+++ LawCommunity/settings.py
@@ -17,13 +17,13 @@ import pymysql
 pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 ROOT_DIR = os.path.dirname(BASE_DIR)
-SECRET_DIR = os.path.join(ROOT_DIR, '.secrets')
+SECRET_DIR = os.path.join(ROOT_DIR, 'LawCommunity/.secrets')
 secrets = json.load(open(os.path.join(SECRET_DIR, 'secrets.json'), 'rb'))
 SECRET_KEY = secrets['SECRET_KEY']
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -120,18 +120,10 @@ WSGI_APPLICATION = 'LawCommunity.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'database',
-        'USER': 'tofusoft',
-        'PASSWORD': secrets['RDS_PASSWORD'],
-        'HOST': secrets['RDS_HOST'],
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': 'SET sql_mode = "STRICT_TRANS_TABLES"'
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
